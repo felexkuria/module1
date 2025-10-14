0dd1e33d5c613512ca19f3cc25573cc7a2ed2684
Felex Kuria <engineerfelex@gmail.com>
2025-10-14 11:53:02 +0300

    Update hello.c to include age variable and enhance output formatting

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
+
 #include <stdio.h>
+
+
 int main()
 {
-    printf("\n\n Hello, World!\n\n\n");
-    return 0;
+  int age=31;
+
+  printf(" Hello, World! %d \n",age);
+  // /n is used to print the new line
+  // /r is used to return the cursor to the beginning of the line
+  // /` is used to print the backslash
+  // /' is used to print the single quote
+  // /" is used to print the double quote
+  // // is used to print the forward slash
+  return 0;
 }
