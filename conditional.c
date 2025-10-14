d4511aed2e07d87733ab005ce56e920c3305b9a5
Felex Kuria <engineerfelex@gmail.com>
2025-10-14 11:58:35 +0300

    Add conditional.c to categorize age groups

---
+++ conditional.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  int age=100;
+
+  if(age<18)
+  {
+    printf("You are a minor.\n");
+  }
+  else if(age>=18 && age<65)
+  {
+    printf("You are an adult.\n");
+  }
+  else
+  {
+    printf("You are a senior citizen.\n");
+  }
+
+  return 0;
+}
