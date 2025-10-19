fe2bfd034928de7072789f865650b88b5b7dabd8
Felex Kuria <engineerfelex@gmail.com>
2025-10-19 10:52:25 +0300

    for loop and while loop

---
+++ for.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (
+    
+){
+
+
+    for (int age = 0; age <= 10; age++)
+    {
+        printf("%d\n", age);
+    }
+    int age = 0;
+    while (age <= 10)
+    {
+        printf("%d\n", age);
+        age++;
+    }
+
+    return 0;
+}
