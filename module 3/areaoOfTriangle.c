e308754500ba041e945ead22350517269e236bec
Felex Kuria <engineerfelex@gmail.com>
2025-10-28 21:17:24 +0300

    Refactor comment for triangleArea function for clarity

--- module 3/areaoOfTriangle.c
+++ module 3/areaoOfTriangle.c
@@ -1,4 +1,6 @@
-// Write a function called triangleArea that calculates and returns the area of a triangle given the base and the height
+// Write a function called triangleArea
+// that calculates and returns the area of a 
+//triangle given the base and the height
 //     Answer : (penalty regime : 10, 20, ... %)
 float triangleArea(float base, float height) {
     float area = (base * height) / 2;
